9559afde89b31698b6afc6026f2235f2ef575c58
Eisner Lopez <elopeza@ucatolica.ac.cr>
2016-06-20 05:08:49 -0600

    Diferentes formas de solucionar el problema
    1. Con tres variables.
    2. Elevado a la potencia.
    3. Mod de la division = 0

--- con_Par_Impar/Program.cs
+++ con_Par_Impar/Program.cs
@@ -10,6 +10,51 @@ namespace con_Par_Impar
     {
         static void Main(string[] args)
         {
+            int n, entero;
+            double dec;
+            Console.WriteLine("Ingrese un #: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            entero = n / 2;
+            dec = (double)n / 2;
+            if (entero - dec == 0)
+            {
+                Console.WriteLine("El numero {0} es par.", n);
+            }
+
+            else
+            {
+                Console.WriteLine("El numero {0} es impar.", n);
+
+            }// fin del if
+
+            if (Math.Pow(-1, n) == 1)
+
+            {
+                Console.WriteLine("El numero {0} es par.", n);
+            }
+
+            else
+            {
+                Console.WriteLine("El numero {0} es impar.", n);
+
+            }// fin del if
+
+            if (n % 2 == 0)
+
+            {
+                Console.WriteLine("El numero {0} es par.", n);
+            }
+
+            else
+            {
+                Console.WriteLine("El numero {0} es impar.", n);
+
+            }// fin del if
+
+            Console.ReadKey();
+
         }
     }
 }
